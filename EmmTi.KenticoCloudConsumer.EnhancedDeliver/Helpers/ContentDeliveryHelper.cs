91a75a6b1650732db9d1bdc42b9454644f47171d
Emmanuel Tissera <emmanuel.tissera@gmail.com>
2016-12-10 13:52:51 +1100

    Added in more ContentDeliveryHelper extensions. Released new nuget package 1.0.4

--- EmmTi.KenticoCloudConsumer.EnhancedDeliver/Helpers/ContentDeliveryHelper.cs
+++ EmmTi.KenticoCloudConsumer.EnhancedDeliver/Helpers/ContentDeliveryHelper.cs
@@ -1,5 +1,6 @@
 ﻿using EmmTi.KenticoCloudConsumer.EnhancedDeliver.Interfaces;
 using KenticoCloud.Deliver;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -10,6 +11,18 @@ namespace EmmTi.KenticoCloudConsumer.EnhancedDeliver.Helpers
     /// </summary>
     public static class ContentDeliveryHelper
     {
+        /// <summary>
+        /// Gets the date time or default value specified.
+        /// </summary>
+        /// <param name="content">The content.</param>
+        /// <param name="elementCode">The element code name.</param>
+        /// <param name="defaultValue">The default value.</param>
+        /// <returns>The date time if the element is not null or the default value if the element is null</returns>
+        public static DateTime GetDateTimeOrDefault(this ContentItem content, string elementCode, DateTime defaultValue = default(DateTime))
+        {
+            return content.Elements[elementCode] == null ? defaultValue : content.GetDateTime(elementCode);
+        }
+
         /// <summary>
         /// Gets a list of modular content.
         /// </summary>
@@ -32,16 +45,88 @@ namespace EmmTi.KenticoCloudConsumer.EnhancedDeliver.Helpers
         }
 
         /// <summary>
-        /// Gets the selected taxonomy.
+        /// Gets the modular content or default.
+        /// </summary>
+        /// <param name="content">The content.</param>
+        /// <param name="elementCode">The element code name.</param>
+        /// <returns>The modular content if the element is not null or the default value if the element is null</returns>
+        public static IEnumerable<ContentItem> GetModularContentOrDefault(this ContentItem content, string elementCode)
+        {
+            return content.Elements[elementCode] == null ? new List<ContentItem>() : content.GetModularContent(elementCode);
+        }
+
+        /// <summary>
+        /// Gets the number or default.
         /// </summary>
         /// <param name="content">The content.</param>
         /// <param name="elementCode">The element code.</param>
+        /// <param name="defaultValue">The default value.</param>
+        /// <returns>The double value if the element is not null or the default value if the element is null</returns>
+        public static double GetNumberOrDefault(this ContentItem content, string elementCode, double defaultValue = 0)
+        {
+            return content.Elements[elementCode] == null ? defaultValue : content.GetNumber(elementCode);
+        }
+
+        /// <summary>
+        /// Gets the selected taxonomy.
+        /// </summary>
+        /// <param name="content">The content.</param>
+        /// <param name="elementCode">The element code name.</param>
         /// <returns>Taxonomy as a Key Value Pair</returns>
         public static KeyValuePair<string, string> GetSelectedTaxonomy(this ContentItem content, string elementCode)
         {
             var element = content.Elements[elementCode];
+            return element == null || element.value.Count == 0 ? new KeyValuePair<string, string>() : new KeyValuePair<string, string>(element.value[0].codename.ToString(), element.value[0].name.ToString());
+        }
 
-            return element != null && element.value != null ? new KeyValuePair<string, string>(element.value[0].codename.ToString(), element.value[0].name.ToString()) : new KeyValuePair<string, string>();
+        /// <summary>
+        /// Gets the string or default.
+        /// </summary>
+        /// <param name="content">The content.</param>
+        /// <param name="elementCode">The element code name.</param>
+        /// <param name="defaultValue">The default value.</param>
+        /// <returns>The string value if the element is not null or the default value if the element is null</returns>
+        public static string GetStringOrDefault(this ContentItem content, string elementCode, string defaultValue = "")
+        {
+            return content.Elements[elementCode] == null ? defaultValue : content.GetString(elementCode);
+        }
+
+        /// <summary>
+        /// Gets the taxonomy items.
+        /// </summary>
+        /// <param name="content">The content.</param>
+        /// <param name="elementCode">The element code name.</param>
+        /// <returns>A Dictionary of taxonomy items</returns>
+        public static Dictionary<string, string> GetTaxonomyItems(this ContentItem content, string elementCode)
+        {
+            var element = content.Elements[elementCode];
+            var taxonomyList = new Dictionary<string, string>();
+
+            if (element == null || element.value == null)
+            {
+                return taxonomyList;
+            }
+
+            foreach (var item in element.value)
+            {
+                taxonomyList.Add(item.codename.ToString(), item.name.ToString());
+            }
+
+            return taxonomyList;
+        }
+
+        /// <summary>
+        /// Maps the content to the Model.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="content">The content.</param>
+        /// <param name="currentDepth">The current depth.</param>
+        /// <returns>Content mapped to the Model</returns>
+        public static T MapContent<T>(this ContentItem content, int currentDepth = 0) where T : IKenticoDeliverViewModel, new()
+        {
+            var model = new T();
+            model.MapContent(content, currentDepth);
+            return model;
         }
 
         /// <summary>
@@ -63,30 +148,6 @@ namespace EmmTi.KenticoCloudConsumer.EnhancedDeliver.Helpers
             }
 
             return filterBuilder.ToString().ToLower();
-        }
-
-        /// <summary>
-        /// Gets the taxonomy items.
-        /// </summary>
-        /// <param name="content">The content.</param>
-        /// <param name="elementCode">The element code.</param>
-        /// <returns>A Dictionary of taxonomy items</returns>
-        public static Dictionary<string, string> GetTaxonomyItems(this ContentItem content, string elementCode)
-        {
-            var element = content.Elements[elementCode];
-            var taxonomyList = new Dictionary<string, string>();
-
-            if (element == null || element.value == null)
-            {
-                return taxonomyList;
-            }
-
-            foreach (var item in element.value)
-            {
-                taxonomyList.Add(item.codename.ToString(), item.name.ToString());
-            }
-
-            return taxonomyList;
         }
     }
 }
